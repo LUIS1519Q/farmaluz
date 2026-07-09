7aed01571eb81e8c24b84f3fe064314a9da832f8
Luis Paspuezán <andres1519q@gmail.com>
2026-07-09 00:54:38 -0500

    fix: restaurar baseURL correcta del EC2 en api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/',
-  // baseURL: 'http://18.225.130.85/api',
+  baseURL: 'http://18.225.130.85/api',
 });
